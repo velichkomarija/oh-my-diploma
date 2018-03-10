625ae3bd17a6df8e6c5e52d870d5a551fa5036db
velichkomarija <vechkomarija4@gmail.com>
2018-03-11 01:36:21 +0300

    Исправлен дизайн введения к зрительному тестированию

--- app/src/main/java/com/example/velickomarija/diploma/TestingEnterImageActivity.java
+++ app/src/main/java/com/example/velickomarija/diploma/TestingEnterImageActivity.java
@@ -5,13 +5,24 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class TestingEnterImageActivity extends AppCompatActivity {
+
+    TextView textView;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_testing_enter_image);
+
+        float size = TextUtils.textSize(getBaseContext());
+        textView =  (TextView) findViewById(R.id.start_text);
+        button = (Button) findViewById(R.id.button_next);
+        button.setTextSize(size);
+        textView.setTextSize(size);
     }
 
     public void onBackPressed() {
