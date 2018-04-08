3f7829aac9e15533641cdd3bd547146c7a436774
velichkomarija <vechkomarija4@gmail.com>
2018-04-08 15:44:53 +0300

    Разметка активити опросника
    Поправка стиля всех радиоботанов.

---
+++ app/src/main/java/com/example/velickomarija/diploma/views/questionnaire/QuestionOneActivity.java
@@ -0,0 +1,15 @@
+package com.example.velickomarija.diploma.views.questionnaire;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.velickomarija.diploma.R;
+
+public class QuestionOneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questionnaire_one);
+    }
+}
