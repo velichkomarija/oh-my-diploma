01c4ca2e2cad9ebe15ced0f4beab9c92f98fe1e6
velichkomarija <vechkomarija4@gmail.com>
2018-04-08 17:55:52 +0300

    Выбор вариантов ответа. Обработка пустого ввода.

--- app/src/main/java/com/example/velickomarija/diploma/views/questionnaire/QuestionOneActivity.java
+++ app/src/main/java/com/example/velickomarija/diploma/views/questionnaire/QuestionOneActivity.java
@@ -3,19 +3,22 @@ package com.example.velickomarija.diploma.views.questionnaire;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.velickomarija.diploma.R;
+import com.example.velickomarija.diploma.models.Functions;
 import com.example.velickomarija.diploma.models.TextUtils;
 
 public class QuestionOneActivity extends AppCompatActivity {
 
     TextView textView;
     RadioGroup radioGroup;
+    Button button;
     int[] arrayResult = new int[20];
-    int res = 1;
+    int res = -1;
     int countQuestions = 1;
 
     @Override
@@ -25,7 +28,9 @@ public class QuestionOneActivity extends AppCompatActivity {
 
         float size = TextUtils.textSize(getBaseContext());
         textView = (TextView) findViewById(R.id.questionText);
-        textView.setTextSize(size*1.3f);
+        button = (Button) findViewById(R.id.button_end_test);
+        textView.setTextSize(size * 1.3f);
+        button.setTextSize(size * 1.3f);
 
         radioGroup = (RadioGroup) findViewById(R.id.radioQuestion);
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
@@ -33,29 +38,27 @@ public class QuestionOneActivity extends AppCompatActivity {
             public void onCheckedChanged(RadioGroup radioGroup, int i) {
                 switch (i) {
                     case -1:
-                        Toast.makeText(getApplicationContext(),
-                                "Ничего не выбрано",
-                                Toast.LENGTH_SHORT).show();
+
                         break;
 
                     case R.id.radioV1:
-                        res=1;
+                        res = 1;
                         break;
 
                     case R.id.radioV2:
-                        res=2;
+                        res = 2;
                         break;
 
                     case R.id.radioV3:
-                        res=3;
+                        res = 3;
                         break;
 
                     case R.id.radioV4:
-                        res=4;
+                        res = 4;
                         break;
 
                     case R.id.radioV5:
-                        res=5;
+                        res = 5;
                         break;
 
                     default:
@@ -76,24 +79,40 @@ public class QuestionOneActivity extends AppCompatActivity {
     }
 
     private void goToNextQuestion(int number) {
+
         if (number < 20) {
             String resource = "q" + number;
+
             try {
-                textView.setText((Integer) R.string.class.getField(resource).get(getResources()));
+                textView.setText((Integer) R.string
+                        .class
+                        .getField(resource)
+                        .get(getResources()));
             } catch (Exception e) {
                 e.printStackTrace();
             }
-            arrayResult[number-2]=res;
+
+            arrayResult[number - 2] = res;
+            res = -1;
         }
-        else{
+        else {
             textView.setText(R.string.q20);
-            arrayResult[19]=res;
-            Toast.makeText(getBaseContext(),"Test all",Toast.LENGTH_LONG);
+            arrayResult[19] = res;
+
+            Toast.makeText(QuestionOneActivity.this,
+                    "Test all",
+                    Toast.LENGTH_LONG).show();
+
         }
     }
 
     public void onClickNextQuestion(View view) {
-        countQuestions++;
-        goToNextQuestion(countQuestions);
+        if (res == -1) {
+            Functions.showErrorDialog(view, "Ничего не выбрано!");
+        } else {
+            countQuestions++;
+            radioGroup.clearCheck();
+            goToNextQuestion(countQuestions);
+        }
     }
 }
