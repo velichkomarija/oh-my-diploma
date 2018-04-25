2cd1e52403e579c736588baed4d0063b1e1be515
velichkomarija <vechkomarija4@gmail.com>
2018-04-26 02:37:33 +0300

    Добавление ProgressBar к опроснику 2

--- app/src/main/java/com/example/velickomarija/diploma/views/questionnaire/QuestionTwoActivity.java
+++ app/src/main/java/com/example/velickomarija/diploma/views/questionnaire/QuestionTwoActivity.java
@@ -2,39 +2,45 @@ package com.example.velickomarija.diploma.views.questionnaire;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.velickomarija.diploma.R;
 import com.example.velickomarija.diploma.models.Algorithms;
 import com.example.velickomarija.diploma.models.TextUtils;
+import com.example.velickomarija.diploma.views.image.TestingImageActivity;
 
 public class QuestionTwoActivity extends AppCompatActivity {
 
     TextView textView;
+    ProgressBar progressBar;
     Button buttonPlus, buttonMinus;
     ImageView image;
     Algorithms algorithm = new Algorithms();
     int[] arrayResult = new int[40];
     float size;
-    Animation animation;
     int countQuestions = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questionnaire_two);
-        animation = AnimationUtils.loadAnimation(this, R.anim.question_anim);
         size = TextUtils.textSize(getBaseContext());
         textView = (TextView) findViewById(R.id.questionText);
         image = (ImageView) findViewById(R.id.image);
         buttonMinus = (Button) findViewById(R.id.button_minus);
         buttonPlus = (Button) findViewById(R.id.button_plus);
+        progressBar = findViewById(R.id.progress);
+
+        progressBar.setVisibility(View.INVISIBLE);
         textView.setTextSize(size * 1.5f);
         buttonMinus.setTextSize(size * 1.3f);
         buttonPlus.setTextSize(size * 1.3f);
@@ -46,16 +52,12 @@ public class QuestionTwoActivity extends AppCompatActivity {
 
     public void onClickNextQuestionPlus(View view) {
         arrayResult[countQuestions - 1] = 1;
-        countQuestions++;
-        view.startAnimation(animation);
-        goToNextQuestion(countQuestions);
+        clk(view);
     }
 
     public void onClickNextQuestionMinus(View view) {
         arrayResult[countQuestions - 1] = 0;
-        countQuestions++;
-        view.startAnimation(animation);
-        goToNextQuestion(countQuestions);
+        clk(view);
     }
 
     private void goToNextQuestion(int number) {
@@ -80,5 +82,34 @@ public class QuestionTwoActivity extends AppCompatActivity {
             intent.putExtra("result", result);
             startActivity(intent);
         }
+    }
+
+    private void clk(View view) {
+        time();
+        countQuestions++;
+        goToNextQuestion(countQuestions);
+    }
+
+    private void time() {
+        textView.setVisibility(View.INVISIBLE);
+        progressBar.setVisibility(View.VISIBLE);
+        buttonMinus.setClickable(false);
+        buttonPlus.setClickable(false);
+        CountDownTimer start = new CountDownTimer(1000, 250) {
+
+            int progress = 25;
+
+            public void onTick(long milliesUntilFinished) {
+                progress = progress + 25;
+                progressBar.setSecondaryProgress(progress);
+            }
+
+            public void onFinish() {
+                progressBar.setVisibility(View.INVISIBLE);
+                textView.setVisibility(View.VISIBLE);
+                buttonMinus.setClickable(true);
+                buttonPlus.setClickable(true);
+            }
+        }.start();
     }
 }
