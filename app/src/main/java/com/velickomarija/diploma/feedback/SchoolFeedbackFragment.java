d8c8068eb6ef4725145173e175bb85e60a183039
velichkomarija <vechkomarija4@gmail.com>
2018-08-11 02:16:19 +0300

    Добавление анкеты

---
+++ app/src/main/java/com/velickomarija/diploma/feedback/SchoolFeedbackFragment.java
@@ -0,0 +1,26 @@
+package com.velickomarija.diploma.feedback;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.velickomarija.diploma.R;
+
+public class SchoolFeedbackFragment extends Fragment{
+
+    private static final String TAG ="SCHOOL_FEEDBACK";
+
+    public SchoolFeedbackFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.school_feedback_fragment, container, false);
+
+        return view;
+    }
+}
